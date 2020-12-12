aa040f95242271f3694ddc3661800c3828ff1b1e
Yunus SARICA <yunusssarica@gmail.com>
2020-12-12 13:29:12 +0300

    Default Hiding With Odin
    Adding hide buttons through odin on editor for panels

--- Assets/Kodluyoruz/Core/UI/Panel.cs
+++ Assets/Kodluyoruz/Core/UI/Panel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 
 [RequireComponent(typeof(CanvasGroup))]
@@ -29,7 +30,7 @@ public class Panel : MonoBehaviour
         }
     }
 
-
+    [Button]
     public void ShowPanel()
     {
         CanvasGroup.alpha = 1;
@@ -37,6 +38,7 @@ public class Panel : MonoBehaviour
         CanvasGroup.blocksRaycasts = true;
     }
 
+    [Button]
     public void HidePanel()
     {
         CanvasGroup.alpha = 0;
