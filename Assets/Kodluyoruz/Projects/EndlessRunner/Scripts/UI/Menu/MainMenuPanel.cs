c211e5cda8370efd8e25408223d216b40b050499
Yunus SARICA <yunusssarica@gmail.com>
2020-12-12 13:26:13 +0300

    Hide On Start
    Main Menu hides when game starts

--- Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/UI/Menu/MainMenuPanel.cs
+++ Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/UI/Menu/MainMenuPanel.cs
@@ -10,14 +10,16 @@ public class MainMenuPanel : Panel
         if (Managers.Instance == null)
             return;
 
-        
+        EventManager.OnGameStart.AddListener(HidePanel);
+        EventManager.OnGameEnd.AddListener(HidePanel);
+
     }
 
     private void OnDisable()
     {
         if (Managers.Instance == null)
             return;
-
-        
+        EventManager.OnGameStart.RemoveListener(HidePanel);
+        EventManager.OnGameEnd.RemoveListener(HidePanel);
     }
 }
