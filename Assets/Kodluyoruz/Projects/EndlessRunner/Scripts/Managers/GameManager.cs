bea7c8fe471fbee535d270b30d088880a85ae516
Yunus SARICA <yunusssarica@gmail.com>
2020-12-12 13:07:23 +0300

    Basic
    Basic Game States and First Screen Loading

--- Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/Managers/GameManager.cs
+++ Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/Managers/GameManager.cs
@@ -23,11 +23,23 @@ public class GameManager : Singleton<GameManager>
 
     public void StartGame()
     {
-        
+        if (IsGameStarted)
+        {
+            return;
+        }
+
+        IsGameStarted = true;
+        EventManager.OnGameStart.Invoke();
     }
 
     public void EndGame()
     {
-        
+        if (!IsGameStarted)
+        {
+            return;
+        }
+
+        IsGameStarted = false;
+        EventManager.OnGameEnd.Invoke();
     }
 }
