7dbf31f09cce4ddd0fd49395f6a36b4d0ebc8c55
Yunus SARICA <yunusssarica@gmail.com>
2020-12-13 11:09:23 +0300

    Before Obstacle Creation Test
    Obstacles created through difficulties and ratios through obstacle manager. Readu for test

--- Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/Managers/ObstacleManager.cs
+++ Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/Managers/ObstacleManager.cs
@@ -38,7 +38,7 @@ public class ObstacleManager : Singleton<ObstacleManager>
     }
 
 
-    public void CreateObstacle()
+    public void SpawnObstacle()
     {
         //Check for is it creating time
         if (Time.time < lastObstacleCreateTime + obstacleCreateWaitTime)
@@ -53,8 +53,41 @@ public class ObstacleManager : Singleton<ObstacleManager>
         if (chance < LevelManager.Instance.DifficulityData.ObstacleSpawnRetrio)
         {
             lastObstacleCreateTime = Time.time; //Last obstacle creted time is now our passed game time
-            EventManager.OnObstacleCreated.Invoke();
+            EventManager.OnObstacleCreated.Invoke(); //Wen invoke this if wont create obstacle
+            return; //We havent pass so we wait for the next time
         }
+
+        //First make list of lane objects
+        List<LaneObject> laneObjects = new List<LaneObject>(TrackManager.Instance.Lanes);
+        laneObjects.Shuffle();
+        //Now er remove lane object from list ake sure our player has one lane without obstacle
+        laneObjects.RemoveAt(Random.Range(0, laneObjects.Count));
+
+        float chanceForAnotherObstacle = Random.Range(0f, 1f);
+
+        lastObstacleCreateTime = Time.time; //Last obstacle creted time is now our passed game time
+
+        for (int i = 0; i < laneObjects.Count; i++)
+        {
+            if (chanceForAnotherObstacle > 0.5f)
+            {
+                CreateObstacle(laneObjects[i].transform.position);
+                chanceForAnotherObstacle = 0f;
+                continue;
+            }
+
+            CreateObstacle(laneObjects[i].transform.position);
+            break;
+        }
+
+        EventManager.OnObstacleCreated.Invoke();
+
+    }
+
+    private GameObject CreateObstacle(Vector3 position)
+    {
+        return Instantiate(LevelManager.Instance.CurrentLevel.GetRandomLevelObject(LevelObjectType.Obstacle),
+            position, Quaternion.identity, TrackManager.Instance.Tracks[TrackManager.Instance.Tracks.Count-1].transform);
     }
 
 }
