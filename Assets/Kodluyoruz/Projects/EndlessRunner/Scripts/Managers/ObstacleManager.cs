29cd26a0a52477563eaf9a4a481217707cb0551b
AhmetBalikcioglu <69127340+AhmetBalikcioglu@users.noreply.github.com>
2020-12-13 15:13:48 +0300

    Last updates

--- Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/Managers/ObstacleManager.cs
+++ Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/Managers/ObstacleManager.cs
@@ -21,7 +21,7 @@ public class ObstacleManager : Singleton<ObstacleManager>
         if (Managers.Instance == null)
             return;
 
-       
+        EventManager.OnLevelStart.AddListener(() => canCreateObstacles = true);
     }
 
     private void OnDisable()
@@ -29,11 +29,17 @@ public class ObstacleManager : Singleton<ObstacleManager>
         if (Managers.Instance == null)
             return;
 
-       
+        EventManager.OnLevelStart.RemoveListener(() => canCreateObstacles = true);
+
     }
 
     private void Update()
     {
+        if(!canCreateObstacles)
+        {
+            lastObstacleCreateTime = Time.time;
+            return;
+        }
         SpawnObstacles();
     }
 
