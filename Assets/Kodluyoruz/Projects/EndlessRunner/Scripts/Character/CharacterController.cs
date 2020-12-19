54ba0c62b0983a31278d173eddf9642db99f9903
Utku <utku.bagdas@gmail.com>
2020-12-19 11:37:41 +0300

    Movement

--- Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/Character/CharacterController.cs
+++ Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/Character/CharacterController.cs
@@ -15,7 +15,7 @@ public class CharacterController : MonoBehaviour, ICharacterController
     {
         if (Managers.Instance == null)
             return;
-
+        EventManager.OnSwipeDetected.AddListener(Move);
        
     }
 
@@ -23,8 +23,8 @@ public class CharacterController : MonoBehaviour, ICharacterController
     {
         if (Managers.Instance == null)
             return;
+        EventManager.OnSwipeDetected.RemoveListener(Move);
 
-       
     }
 
     /// <summary>
@@ -34,17 +34,42 @@ public class CharacterController : MonoBehaviour, ICharacterController
     /// <param name="direction"></param>
     public void Move(Vector3 direction)
     {
-
+       
     }
 
     public void Move(Swipe swipe, Vector2 direction)
     {
-        
-        
+        Debug.Log(swipe);
+        if (!Character.IsControlable)
+            return;
 
+        LaneObject laneObject = CurrentLane.GetLane(swipe);
+
+        switch (swipe)
+        {
+           
+            case Swipe.Up:
+                Jump();
+                break;
+            case Swipe.Down:
+                Slide();
+                break;
+            case Swipe.Left:
+                if (laneObject == null)
+                    return;
+                JumpToLane(laneObject);
+                break;
+            case Swipe.Right:
+                if (laneObject == null)
+                    return;
+                JumpToLane(laneObject);
+                break;
+
+            default:
+                break;
+        }
     }
-
-    public void Jump()
+        public void Jump()
     {
         
     }
@@ -53,4 +78,11 @@ public class CharacterController : MonoBehaviour, ICharacterController
     {
        
     }
-}
+
+    private void JumpToLane(LaneObject laneObject)
+    {
+    transform.DOJump(new Vector3(laneObject.transform.position.x, transform.position.y, transform.position.z), 1f, 0, 0.3f);
+                Character.OnCharacterSwitchLane.Invoke();
+    }
+
+    }
