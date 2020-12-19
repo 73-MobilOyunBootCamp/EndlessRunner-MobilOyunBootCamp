1bf5d849941d9f6c0b7af1b5e900daf4251032be
ladylovelacee <bgizembulut@gmail.com>
2020-12-19 12:22:45 +0300

    Update CharacterController.cs

--- Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/Character/CharacterController.cs
+++ Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/Character/CharacterController.cs
@@ -16,7 +16,7 @@ public class CharacterController : MonoBehaviour, ICharacterController
         if (Managers.Instance == null)
             return;
 
-       
+        EventManager.OnSwipeDetected.AddListener(Move);
     }
 
     private void OnDisable()
@@ -24,7 +24,7 @@ public class CharacterController : MonoBehaviour, ICharacterController
         if (Managers.Instance == null)
             return;
 
-       
+        EventManager.OnSwipeDetected.RemoveListener(Move);
     }
 
     /// <summary>
@@ -39,18 +39,68 @@ public class CharacterController : MonoBehaviour, ICharacterController
 
     public void Move(Swipe swipe, Vector2 direction)
     {
-        
-        
+        if (!Character.IsControlable)
+            return;
 
+        LaneObject laneObject = CurrentLane.GetLane(swipe);
+
+        switch (swipe)
+        {
+            case Swipe.Up:
+                Jump();
+                break;
+            case Swipe.Down:
+                break;
+            case Swipe.Left:
+
+                if (laneObject == null)
+                    return;
+
+                JumpToLane(laneObject);
+                
+                break;
+            case Swipe.Right:
+                if (laneObject == null)
+                    return;
+
+                JumpToLane(laneObject);
+
+                break;
+            default:
+                break;
+        }
     }
-
+    
     public void Jump()
     {
-        
+        if (isJumping)
+            return;
+
+        isJumping = true;
+        transform.DOJump(transform.position, 2.5f, 1, 1.2f)
+            .SetEase(Ease.InOutSine)
+            .OnComplete(() => isJumping = false);
+        Character.OnCharacterJump.Invoke();
+        //Character.Collider.enabled = false;
     }
 
     public void Slide()
     {
-       
+        Character.OnCharacterSlide.Invoke();
+        //Character.Collider.enabled = false;
     }
+
+    bool isJumping;
+    void JumpToLane(LaneObject laneObject)
+    {
+        if (isJumping)
+            return;
+
+        isJumping = true;
+        transform.DOJump(new Vector3(laneObject.transform.position.x, transform.position.y, transform.position.z), 1f, 1, 0.3f)
+            .OnComplete(() => isJumping = false);
+        
+        Character.OnCharacterSwitchLane.Invoke();
+    }
+
 }
