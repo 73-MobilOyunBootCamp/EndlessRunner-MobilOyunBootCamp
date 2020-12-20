3a1cb806d63fa53b4048fa0bf43ffefcc090b3f4
Muhammed Üşenmez <mhusenmez@gmail.com>
2020-12-20 11:59:40 +0300

    HealthPanel

--- Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/UI/Menu/HealthPanel.cs
+++ Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/UI/Menu/HealthPanel.cs
@@ -14,18 +14,29 @@ public class HealthPanel : Panel
     {
         if (Managers.Instance == null)
             return;
-
+        EventManager.OnPlayerDataUpdated.AddListener(UpdateHealth);
     }
 
     private void OnDisable()
     {
         if (Managers.Instance == null)
             return;
-
+        EventManager.OnPlayerDataUpdated.RemoveListener(UpdateHealth);
     }
 
     private void UpdateHealth(PlayerData playerData)
     {
-       
+        if (playerData.CurrentHelath > HeartImages.Count)
+            return;
+
+        foreach (var item in HeartImages)
+        {
+            item.enabled = false;
+        }
+
+        for (int i = 0; i < playerData.CurrentHelath; i++)
+        {
+            HeartImages[i].enabled = true;
+        }
     }
 }
