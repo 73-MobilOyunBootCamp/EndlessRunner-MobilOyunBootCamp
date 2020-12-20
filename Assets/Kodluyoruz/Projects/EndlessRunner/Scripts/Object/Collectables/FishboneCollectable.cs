55bc9721feb0bd77c362cd6411f450f760c7cff4
IdrisDindar <idrisdindar@outlook.com>
2020-12-20 16:00:28 +0300

    Powerup

--- Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/Object/Collectables/FishboneCollectable.cs
+++ Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/Object/Collectables/FishboneCollectable.cs
@@ -18,13 +18,32 @@ public class FishboneCollectable : CollectableBase
     public override void Use()
     {
         var playerData = SaveLoadManager.LoadPDP<PlayerData>(SavedFileNameHolder.PlayerData, new PlayerData());
-        playerData.CoinAmount += 1;
+        playerData.CoinAmount += 1 * GameManager.Instance.GameData.PointMultiplier;
         SaveLoadManager.SavePDP(playerData, SavedFileNameHolder.PlayerData);
         Dispose();
     }
 
     private void Update()
     {
-        
+        if (!GameManager.Instance.GameData.IsMagnetActive && !isStarted)
+            return;
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 other = CharacterManager.Instance.Player.transform.position - transform.position;
+
+        if (Vector3.Dot(forward, other) > 0)
+        {
+            return;
+        }
+
+        float distanceToPlayer = Vector3.Distance(transform.position, CharacterManager.Instance.Player.transform.position);
+
+        if (distanceToPlayer < 10f)
+        {
+            isStarted = true;
+            float speed = 30 - distanceToPlayer;
+            speed = speed * Time.deltaTime * 0.5f;
+            transform.position = Vector3.MoveTowards(transform.position, CharacterManager.Instance.Player.transform.position + Vector3.up, speed);
+        }
     }
 }
--- Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/PowerUps/HealthPowerUp.cs
+++ Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/PowerUps/HealthPowerUp.cs
@@ -13,6 +13,11 @@ public class HealthPowerUp : PowerUpBase
 
     public override void Use()
     {
-        
+        CharacterManager.Instance.Player.GetComponent<IHealable>().Heal();
+        Instantiate(PowerUpDisplayPrefab,
+            CharacterManager.Instance.Player.transform.position,
+            Quaternion.identity, CharacterManager.Instance.Player.transform);
+
+        Dispose();
     }
 }
