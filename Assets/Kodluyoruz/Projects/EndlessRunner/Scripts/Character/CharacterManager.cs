c60d41a030f9971ddf9634b0cede2554c0a7e617
enkalados <yakup_mknc@hotmail.com>
2020-12-20 13:59:01 +0300

    Coin Collect

--- Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/Character/CharacterManager.cs
+++ Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/Character/CharacterManager.cs
@@ -65,11 +65,17 @@ public class CharacterManager : Singleton<CharacterManager>
     
     public void AddCharacter(Character character)
     {
-        
+        if (!characters.Contains(character))
+        {
+            Characters.Add(character);
+        }
     }
 
     public void RemoveCharacter(Character character)
     {
-        
+        if (characters.Contains(character))
+        {
+            Characters.Remove(character);
+        }
     }
 }
--- Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/Managers/TrackManager.cs
+++ Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/Managers/TrackManager.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -151,6 +150,10 @@ public class TrackManager : Singleton<TrackManager>
         Tracks.Remove(trackObject);
         Destroy(trackObject.gameObject);
     }
+    public TrackObject GetLastTrackObject()
+    {
+        return Tracks[Tracks.Count - 1];
+    }
     #endregion
 
     #region Lanes
@@ -184,6 +187,10 @@ public class TrackManager : Singleton<TrackManager>
         }
 
         return closestLane;
+    }
+    public LaneObject GetRandomLane()
+    {
+        return Lanes[Random.Range(0, Lanes.Count)];
     }
     #endregion
 }
