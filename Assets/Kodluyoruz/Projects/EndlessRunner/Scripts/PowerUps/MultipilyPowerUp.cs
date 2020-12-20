abbb0d371d9f57ecfd4985a04299e384a49e18e5
AhmetBalikcioglu <69127340+AhmetBalikcioglu@users.noreply.github.com>
2020-12-20 16:10:56 +0300

    +add Health, Magnet, Multiply PowerUp

--- Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/PowerUps/MultipilyPowerUp.cs
+++ Assets/Kodluyoruz/Projects/EndlessRunner/Scripts/PowerUps/MultipilyPowerUp.cs
@@ -6,21 +6,33 @@ using Sirenix.OdinInspector;
 
 public class MultipilyPowerUp : PowerUpBase
 {
-    private GameObject effect;
     public override IEnumerator ExecuteCo()
     {
+        effect = Instantiate(PowerUpDisplayPrefab,
+            CharacterManager.Instance.Player.transform.position,
+            Quaternion.identity,
+            CharacterManager.Instance.Player.transform);
+
+        GameManager.Instance.GameData.PointMultiplier = 2;
+        yield return new WaitForSeconds(GameManager.Instance.GameData.MultiplyTimer);
+        GameManager.Instance.GameData.PointMultiplier = 1;
+        Destroy(effect);
+        Destroy(this);
         yield return null;
 
     }
 
     public override void Use()
     {
-        
+        MultipilyPowerUp multipilyPowerUp = CharacterManager.Instance.Player.gameObject.AddComponent<MultipilyPowerUp>();
+        multipilyPowerUp.Initialize(this);
+        multipilyPowerUp.Execute();
+        Dispose();
     }
 
     public override void Interup()
     {
         base.Interup();
-        
+        GameManager.Instance.GameData.PointMultiplier = 1;
     }
 }
